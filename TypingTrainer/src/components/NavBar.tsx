64e6be305f9e833f902c65e60e59f935da125751
Daniel Kirushin <retrostyae@mail.ru>
2024-08-04 00:15:13 +0300

    Clean-rep//install>>all

---
+++ TypingTrainer/src/components/NavBar.tsx
@@ -0,0 +1,22 @@
+
+
+type Props = {}
+
+export function NavBar(props: Props) {
+  return (
+    <section className="header-nav-bar">
+      <div className="logo-section">
+        <p>
+          Typing Speed Trainer
+        </p>
+      </div>
+      <div className="option-section">
+          <p>Home</p>
+          <p>About</p>
+      </div>
+
+
+    </section>
+  )
+}
+
